c7557d443d9fc14c73b0aab586f78ac7ebef71c6
helpfulrobot <chris@silverstripe.com>
2016-01-01 06:14:49 +1300

    Converted to PSR-2

--- code/groupdocsDotNetAnnotation.php
+++ code/groupdocsDotNetAnnotation.php
@@ -1,21 +1,22 @@
 <?php
 
-class groupdocsDotNetAnnotation {
-	
-	public static function handle_shortcode($arguments, $url, $parser, $shortcode) {
-
+class groupdocsDotNetAnnotation
+{
+    
+    public static function handle_shortcode($arguments, $url, $parser, $shortcode)
+    {
         $width = 600 ;
-		$height = 800;
+        $height = 800;
 
 
-		if(isset($arguments['width']) && !empty($arguments['width'])) {
-			$width = strip_tags(trim($arguments['width']));
-		}
+        if (isset($arguments['width']) && !empty($arguments['width'])) {
+            $width = strip_tags(trim($arguments['width']));
+        }
 
-		if(isset($arguments['height']) && !empty($arguments['height'])) {
-			$height = strip_tags(trim($arguments['height']));
-		}
-        if(isset($arguments['user_name']) && !empty($arguments['user_name'])) {
+        if (isset($arguments['height']) && !empty($arguments['height'])) {
+            $height = strip_tags(trim($arguments['height']));
+        }
+        if (isset($arguments['user_name']) && !empty($arguments['user_name'])) {
             $height = strip_tags(trim($arguments['user_name']));
         }
 
@@ -23,6 +24,5 @@ class groupdocsDotNetAnnotation {
 
         $grpDotNetAnnotation = new groupdocsDotNetAnnotation_Result($width, $height, $url, $user_name);
         return $grpDotNetAnnotation->forTemplate();
-
-	}
+    }
 }
